f7da444d08275dbe2197784cc5135507a7e3ea42
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2020-10-24 08:30:25 +0200

    Revert solution for converting bool and int

--- src/pyload/core/api/__init__.py
+++ src/pyload/core/api/__init__.py
@@ -155,8 +155,6 @@ class Api:
         """
         if section == "core":
             value = self.pyload.config[category][option]
-            if self.pyload.config.config[category][option]['type'] in ['bool', 'int']:
-                return value
         else:
             value = self.pyload.config.get_plugin(category, option)
         return str(value)

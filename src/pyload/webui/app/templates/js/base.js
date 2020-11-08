f6a483971912d4711f5fba0354df0cbc0ca8d51f
Ozzieisaacs <ozzie.fernandez.isaacs@googlemail.com>
2020-11-08 12:16:38 +0100

    Rename active to actives (as for pyplex)

--- src/pyload/webui/app/templates/js/base.js
+++ src/pyload/webui/app/templates/js/base.js
@@ -122,9 +122,9 @@ document.addEvent("domready", function() {
 
 var LoadJsonToContent = function(data) {
     $("speed").set('text', humanFileSize(data.speed)+"/s");
-    $("active").set('text', data.active);
-    $("active_from").set('text', data.queue);
-    $("active_total").set('text', data.total);
+    $("actives").set('text', data.active);
+    $("actives_from").set('text', data.queue);
+    $("actives_total").set('text', data.total);
 
     if (data.captcha) {
         if ($("cap_info").getStyle("display") !== "inline") {

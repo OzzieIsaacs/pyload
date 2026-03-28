46f6952000400a37990516d206f91272572d03e4
Ozzie Isaacs <ozzie.fernandes.isaacs@googlemail.com>
2026-03-28 09:48:44 +0100

    Update OneFichier

--- src/pyload/plugins/downloaders/OneFichierCom.py
+++ src/pyload/plugins/downloaders/OneFichierCom.py
@@ -6,7 +6,7 @@ from ..base.simple_downloader import SimpleDownloader
 class OneFichierCom(SimpleDownloader):
     __name__ = "OneFichierCom"
     __type__ = "downloader"
-    __version__ = "1.22"
+    __version__ = "1.23"
     __status__ = "testing"
 
     __pattern__ = r"https?://(?:www\.)?(?:(?P<ID1>\w+)\.)?(?P<HOST>1fichier\.com|alterupload\.com|cjoint\.net|d(?:es)?fichiers\.com|dl4free\.com|megadl\.fr|mesfichiers\.org|piecejointe\.net|pjointe\.com|tenvoi\.com)(?:/\?(?P<ID2>\w+))?"
@@ -45,7 +45,7 @@ class OneFichierCom(SimpleDownloader):
     NAME_PATTERN = r'<td class="normal"><span style="font-weight:bold">(?P<N>.+?)<'
     SIZE_PATTERN = r"<span .*>(?P<S>[\d.,]+) (?P<U>[\w^_]+)</span></td>"
     OFFLINE_PATTERN = r"(?:File not found !\s*<|>\s*The requested file (?:has been deleted|do(?:es)? not exist))"
-    LINK_PATTERN = r'<a href="(.+?)".*>Click here to download the file</a>'
+    LINK_PATTERN = r'<a href="(.+?)".*>Start your download</a>'
     TEMP_OFFLINE_PATTERN = r"Without subscription, you can only download one file at|Our services are in maintenance"
     PREMIUM_ONLY_PATTERN = r"is not possible to unregistered users|need a subscription"
 
